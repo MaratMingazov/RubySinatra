12d2cd1f2e90261f0650f9ad7400e92480cef9de
MaratMingazov <maratmingazovr@gmail.com>
2017-05-03 12:08:43 +0300

    Add list of movies

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require 'sinatra'
 require 'movie'
 
 get('/movies') do
-    @movie = Movie.new
-    @movie.title = "Jaws"
+    @movies = []
+    @movies[0] = Movie.new
+    @movies[0].title = "Jaws"
+    @movies[1] = Movie.new
+    @movies[1].title = "Alien"
+    @movies[2] = Movie.new
+    @movies[2].title = "Terminator 2"
     erb :index
 end
 
